c7e5842bdba397858cf5d91447becc67035d6489
Maxim Volgin <maxim.volgin@me.com>
2020-10-02 11:52:38 +0200

    data

--- Sources/ThyPorch/MPSGData.swift
+++ Sources/ThyPorch/MPSGData.swift
@@ -14,15 +14,17 @@ public struct MPSGData {
     public static func generate(parameterCount: Int, dataType: MPSDataType, minimum: Float, maximum: Float) -> Data {
         switch dataType {
         case .float32:
-            let parameterArray = MPSGData.getRandomData(parameterCount: parameterCount, minimum: minimum, maximum: maximum)
+            let parameterArray = MPSGData.getRandomData(count: parameterCount, minimum: minimum, maximum: maximum)
             return Data(bytes: parameterArray, count: parameterCount * MemoryLayout<Float32>.size)
         default:
             fatalError("Not implemented for `\(dataType)` yet.")
         }
     }
     
-    private static func getRandomData(parameterCount: Int, minimum: Float32, maximum: Float32) -> [Float32] {
-        return (1...parameterCount).map { _ in Float32.random(in: minimum...maximum) }
+    private static func getRandomData(count: Int, minimum: Float32, maximum: Float32) -> [Float32] {
+        minimum == maximum ?
+            [Float32](repeating: minimum, count: count) :
+            (1...count).map { _ in Float32.random(in: minimum...maximum) }
     }
 
 }
