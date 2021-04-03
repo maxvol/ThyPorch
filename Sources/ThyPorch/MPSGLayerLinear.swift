87aa99ebcf33c03d84f1241660b7e88ace0aa742
Maxim Volgin <>
2021-04-03 19:51:48 +0200

    wip

--- Sources/ThyPorch/MPSGLayerLinear.swift
+++ Sources/ThyPorch/MPSGLayerLinear.swift
@@ -1,5 +1,5 @@
 //
-//  MPSGLayerDense.swift
+//  MPSGLayerLinear.swift
 //  
 //
 //  Created by Maxim Volgin on 03/04/2021.
@@ -11,24 +11,21 @@ import MetalPerformanceShaders
 import MetalPerformanceShadersGraph
 
 @available(macOS 11, iOS 14, *)
-public struct MPSGLayerDense: MPSGLayer {
+public struct MPSGLayerLinear: MPSGLayer {
     public let graph: MPSGraph
     public private(set) var variableData: [MPSGModel.VariableData]
     let weightVariableData: MPSGModel.VariableData
     let biasVariableData: MPSGModel.VariableData
-    let hasActivation: Bool
     let name: String?
     
     public init(graph: MPSGraph,
                 weightsShape: Shape,
-                hasActivation: Bool,
                 name: String? = nil
     ) {
         self.graph = graph
         self.weightVariableData = Self.add(layerVariable: .weight(layerShape: weightsShape, layerName: name), to: graph)
         self.biasVariableData = Self.add(layerVariable: .bias(layerShape: weightsShape, layerName: name), to: graph)
         self.variableData = [weightVariableData, biasVariableData]
-        self.hasActivation = hasActivation
         self.name = name
     }
         
@@ -39,12 +36,7 @@ public struct MPSGLayerDense: MPSGLayer {
         let fcBiasTensor = graph.addition(fcTensor,
                                           biasVariableData.tensor,
                                           name: nil)
-        if !hasActivation {
-            return fcBiasTensor
-        }
-        let fcActivationTensor = graph.reLU(with: fcBiasTensor,
-                                            name: nil)
-        return fcActivationTensor
+        return fcBiasTensor
     }
     
 }
